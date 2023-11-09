0774bfdf0e50283184396b3aea1f6fe90e67bd38
Gyuhyeok <ghkdrbgur13@naver.com>
2023-11-09 14:22:23 +0900

    chore : 패키지 위치 수정

--- src/main/java/umc/spring/domain/enums/MemberStatus.java
+++
@@ -1,6 +0,0 @@
-package umc.spring.domain.enums;
-
-public enum MemberStatus {
-
-    ACTIVE, INACTIVE
-}
--- src/main/java/umc/spring/domain/enums/SocialType.java
+++
@@ -1,6 +0,0 @@
-package umc.spring.domain.enums;
-
-public enum SocialType {
-
-    KAKAO, GOOGLE, NAVER, APPLE
-}
